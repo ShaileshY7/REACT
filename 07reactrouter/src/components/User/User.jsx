e24383b257bc24e2b4e1ba0f70686e226db3144a
Shailesh Yadav <shailesh772002@gmail.com>
2025-07-30 20:10:09 +0530

    context Api

--- 07reactrouter/src/components/User/User.jsx
+++ 07reactrouter/src/components/User/User.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const User = () => {
+function User ()  {
+  const {userid}=useParams()
   return (
-    <div>
-       User:
+    <div className='bg-gray-600 text-3xl text-white p-4'>
+       User:{userid}
     </div>
   )
 }
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider ,Route} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import Github  from './components/Github/Github.jsx'
+import Github ,{githubInfoLoader} from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
 // const router=createBrowserRouter([
 //   {
 //     path: '/',
@@ -38,10 +39,12 @@ import Github  from './components/Github/Github.jsx'
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
-      <Route path="" element={<Home/>}/>
+      <Route index element={<Home/>}/>
       <Route path="about" element={<About/>}/>
       <Route path="contact" element={<Contact/>}/>
-      <Route path="user/:id" element={<User/>}/>
+      <Route path="github" element={<Github/>} loader={githubInfoLoader}/>
+      <Route path="user/:userid" element={<User/>}/>
+       <Route path="*" element={<h1>404 - Page Not Found</h1>} />
     </Route>
   )
 )
